579657bdfe1e28607c192d2303b462e1f416d222
group001 <group001@infoway.com>
2019-06-15 08:14:53 +0530

    exam repostory

---
+++ kalyani123/src/test/java/com/example/kalyani123/Kalyani123ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kalyani123;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kalyani123ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
